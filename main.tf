2f130ca194c2c463c918f1c47c13e76b7b8b60b4
rosaelen <118381364+rosaelen@users.noreply.github.com>
2023-06-30 12:02:18 -0600

    Update main.ft format & added file .gitignore

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws"{
 }
 
 resource "aws_instance" "example" {
-	ami				= "ami-0fb653ca2d3203ac1"
+	ami		= "ami-0fb653ca2d3203ac1"
 	instance_type	= "t2.micro"
 	tags = {
 		Name = "terraform-example"
