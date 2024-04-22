66cbb2893d49b42d44a1c9439e457185ba83f3ed
rosaelen <rossie.cloud@gmail.com>
2024-04-21 22:02:51 -0600

    avance

---
+++ gcp/original/variables.tf
@@ -0,0 +1,50 @@
+variable "project" {
+  description = "Project ID"
+  type        = string
+  default     = "perfect-day-417915"
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Zone"
+  type        = string
+  default     = "us-central1-c"
+}
+
+variable "name" {
+  description = "prefix for all resources created in this example"
+  type        = string
+  default     = "examplet"
+}
+
+variable "cloud_run_name" {
+  description = "Cloud Run Name"
+  type        = string
+  default     = "helloworld-python"
+}
+
+/*
+variable "external_ip" {
+  description = "Reserved global IPv4"
+  type        = string
+  default     = "34.36.219.212"
+}
+*/
+
+variable "ssl_certificate" {
+  description = "Self-signed SSL Certficate"
+  type        = string
+  default     = "run-ssl-certificate"
+}
+
+
+variable "dns_zone" {
+  description = "The DNS zone you'll add the IP address to"
+  type        = string
+  default     = "my-public-zone"
+}
---
+++ gcp/self-signed-cert/main.tf
@@ -0,0 +1,43 @@
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = "./app"
+  output_path = "./app/function.zip"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket-${var.name}-${random_id.ids.dec}"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "zip" {
+  source       = data.archive_file.source.output_path
+  content_type = "application/zip"
+
+  name   = "src-${var.name}-${random_id.ids.dec}.zip"
+  bucket = google_storage_bucket.bucket.name
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name    = "${var.name}-function"
+  runtime = "python312"
+  region  = var.region
+
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.zip.name
+  entry_point           = "home"
+  trigger_http          = true
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
+resource "random_id" "ids" {
+  byte_length = 4
+}
